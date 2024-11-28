52eb5f7fa0e4b396379dacbad94d814f72d8f2ff
SOUMITRI ROY <soumitriroy05@gmail.com>
2024-11-28 20:27:54 +0530

    Update 1.10.py

--- pythonday1.py/1.10.py
+++ pythonday1.py/1.10.py
@@ -1,3 +1,5 @@
+#WAP to enter your name in lower case and convert it into upper case and vice versa.
+
 lowercase_name="soumitri"
 uppercase_name=lowercase_name.swapcase()
 print("Uppercase name:", uppercase_name)
