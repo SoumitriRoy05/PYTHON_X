4fcc3dd510cdc8f1dc9db330455dbd04a6b25d74
SOUMITRI ROY <soumitriroy05@gmail.com>
2024-11-28 20:22:09 +0530

    Update 1.2.py

--- pythonday1.py/1.2.py
+++ pythonday1.py/1.2.py
@@ -1,8 +1,10 @@
+#WAP to input principal amount, rate of interest and time then calculate simple interest and cumulative interest
+
 c_amount=int(input("enter amount:"))
 rate=float(input("enter the rate:"))
 time=int(input("enter time:"))
 
 amount=c_amount*(pow((1+rate/100),time))
 c_interest=amount-c_amount
-print("Compound interest:",c_interest)
-print("simple interest:",(c_amount*rate*time)/100)
+print(f"Compound interest:{c_interest}")
+print("simple interest:{(c_amount*rate*time)/100}")
