002114790aa2e0e9ffad83c3d4db67554c8584a4
SOUMITRI ROY <soumitriroy05@gmail.com>
2024-11-28 20:23:01 +0530

    Update 1.3.py

--- pythonday1.py/1.3.py
+++ pythonday1.py/1.3.py
@@ -1,5 +1,7 @@
+#WAP to input principal amount, interest and time then calculate rate of interest.
+
 p_amount=int(input("Enter amount:"))
 time=int(input("enter time:"))
 rate=float(input("Enter interest:"))
 rate_of_interest=(rate*100)/(p_amount*time)
-print("Rate of interest:", rate_of_interest)
+print(f"Rate of interest:{rate_of_interest}")
