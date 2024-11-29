520bba46e15f77695381d6836d434ff0bf615927
SOUMITRI ROY <soumitriroy05@gmail.com>
2024-11-30 00:47:20 +0530

    Update 1.4.py

--- pythonday_1/1.4.py
+++ pythonday_1/1.4.py
@@ -1,3 +1,4 @@
+#WAP to input principal amount, rate of interest and interest then calculate time
 p_amount=int(input("Enter amount:"))
 rate_of_interest=int(input("enter rate of interest:"))
 rate=int(input("Enter rate:"))
