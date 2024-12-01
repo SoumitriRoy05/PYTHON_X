6fa568b1244736a0c3480a759ae9a62291d45c88
SOUMITRI ROY <soumitriroy05@gmail.com>
2024-12-01 11:46:17 +0530

    Update 2.2.py

--- pythonday_2/2.2.py
+++ pythonday_2/2.2.py
@@ -1,3 +1,5 @@
+#Write a Python program to enter a 6-digit number (for example 786531) find the sum of their increasing order powers (sum1=71+82+63+54+35+16)  and reverse the summation of digits (sum2=11+32+53+64+85+76) . find subtraction of sum2 from sum1 and print the result
+
 num = input("Enter a 6-digit number: ")
 if len(num) != 6 or not num.isdigit():
     print("Enter a valid 6-digit number!")
