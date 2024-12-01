b165daacb758d15515857fafdc0091aab94ce710
SOUMITRI ROY <soumitriroy05@gmail.com>
2024-12-01 11:47:50 +0530

    Delete pythonday2.py/2.1.py

--- pythonday2.py/2.1.py
+++
@@ -1,8 +0,0 @@
-#Write a Python program to perform addition and subtraction of two complex number to be inputted from standard keyboard.
-
-num1=complex(input("Enter a complex number:"))
-num2=complex(input("Enter a complex number:"))
-sum=num1+num2
-sub=num1-num2
-print("Sum is",sum)
-print("Difference is:", sub)
