664d66e67e7bcb4cb7f49aff60698a6f615824d6
SOUMITRI ROY <soumitriroy05@gmail.com>
2024-12-05 19:56:13 +0530

    Update q1.py

--- pythonday_6/q1.py
+++ pythonday_6/q1.py
@@ -1,3 +1,6 @@
+#Write a programme in Python using function to execute the given tree by displaying the values of statements present in the level1 and whenever the value of n is equals to 0 the function will get exit and return 0 (call the function foo with the value (3042, 0) and foo will call itself by the updated value of n and p).
+#[Output of this programme will be the value of K, L, and P for each iteration of function foo until the condition is true.]
+
 def foo(n, P):
     if n == 0:
         return P
