53b937cb790f08b1c0f8dd46414d28694d5a2366
SOUMITRI ROY <soumitriroy05@gmail.com>
2024-12-05 19:57:19 +0530

    Update q4.1.py

--- pythonday_6/q4.1.py
+++ pythonday_6/q4.1.py
@@ -1,4 +1,6 @@
+#Write a programme in Python to check a user inputted string is palindrome
 #with function(palindrome)
+
 def isPalindrome(str):
     for i in range (0,int(len(str)/2)):
         if str[i]!=str[len(str)-i-1]:
