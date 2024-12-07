ab1d7534363b83c62ca9898c548e3cadc05380e2
SOUMITRI ROY <soumitriroy05@gmail.com>
2024-12-08 00:29:23 +0530

    Update q4.py

--- pythonday_9/q4.py
+++ pythonday_9/q4.py
@@ -1,3 +1,4 @@
+#Write a program to wipe out the contents of a file using python. [Create that text file named as planets.txt that contains list of planets in the solar system]
 
 f=open("planets.txt","w")
 f.write("Mercury\nVenus\nEarth\nMars\nJupiter\nSaturn\nUranus\nNeptune")
