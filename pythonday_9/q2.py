730eb803a9ae70dfbcb12d013148da735fc54115
SOUMITRI ROY <soumitriroy05@gmail.com>
2024-12-08 00:28:25 +0530

    Update q2.py

--- pythonday_9/q2.py
+++ pythonday_9/q2.py
@@ -1,3 +1,5 @@
+#Write a python program to open three files planets1.txt, planets2.txt and planets3.txt. If any of these files are not present, a message without exiting the program must be printed promoting the same
+
 files = ["planets1.txt", "planets2.txt", "planets3.txt"]
 
 for file in files:
