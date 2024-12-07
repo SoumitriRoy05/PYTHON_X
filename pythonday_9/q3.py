9fe4af4fc9febe2ed4c930320f34af2757b3cdd1
SOUMITRI ROY <soumitriroy05@gmail.com>
2024-12-08 00:28:58 +0530

    Update q3.py

--- pythonday_9/q3.py
+++ pythonday_9/q3.py
@@ -1,3 +1,4 @@
+#Write a program to make a copy of a text file “Galaxy.txt”. [Galaxy.txt contains minimum 25 galaxies name those are discover by NASA Hubble space telescope]
 
 f=open("Galaxy.txt","w")
 f.write("Andromeda\nMilky Way\nTriangulum\nWhirlpool\n...\n")
