29f36f5344fb7ac73d7e07ff94a0c15b397e6916
SOUMITRI ROY <soumitriroy05@gmail.com>
2024-12-09 13:56:27 +0530

    Delete pythonday10/q1.py

--- pythonday10/q1.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter the value of a: "))
-b = int(input("Enter the value of b: "))
-try:
-    result = a / b
-    print(f"Result of a/b:,{ result}")
-except ZeroDivisionError:
-    print("Result of a/b: infinite")
